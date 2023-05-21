f586d840beefa0b9f87c86a4a1eca8308a6deb60
Shakibzeinal <132278832+Shakibzeinal@users.noreply.github.com>
2023-05-21 10:06:48 +0330

    Add files via upload

---
+++ car/ConsoleApplication8/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApplication8
+{
+    class Car
+    {
+        private string model;
+        private string rang;
+        private int year;
+        private int soraat;
+
+
+        public Car(string mod, string color, int year, int speed)
+        {
+            set_model(mod);
+            set_rang(color);
+            set_soraat(speed);
+        }
+
+        public void set_model(string mod_)
+        {
+            this.model = mod_;
+        }
+        public void set_rang(string color)
+        {
+            this.rang = color;
+        }
+        public void set_year(int y)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            int curr_year = pc.GetYear(System.DateTime.Now);
+            this.year = curr_year - y;
+        }
+
+        public void set_soraat(int speed)
+        {
+            this.soraat = speed;
+
+        }
+
+
+        public string get_model()
+        {
+            return model;
+        }
+        public string get_rang()
+        {
+            return rang;
+        }
+        public int get_year()
+        {
+            return year;
+        }
+        public int get_soraat()
+        {
+            return soraat;
+        }
+
+        public string Who()
+        {
+            return "rang=" + get_rang() + "  model=" + get_model() + "year=" + get_model() + "  soraat=" + get_soraat();    
+        }
+    }
+}
---
+++ car/ConsoleApplication8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Model : ");
+            string model = Console.ReadLine();
+            Console.Write("Enter Rang : ");
+            string rang = Console.ReadLine();
+            Console.Write("Enter Speed : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+
+            Car st = new Car(model,rang,speed);
+            //st.set_color(rang);
+            //st.set_mod(model);
+            //st.set_speed(soraat);
+
+
+            //Console.WriteLine(
+            //    st.get_rang() + "  " +
+            //    st.get_model() + "  " + 
+            //    st.get_speed());
+
+            Console.WriteLine(st.Who());
+            Console.ReadKey();
+        }
+    }
+}
